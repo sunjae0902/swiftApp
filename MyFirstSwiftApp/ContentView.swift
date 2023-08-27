83b1d83b6e136ccc9b51acc609a725716faf5f99
sunjae0902 <sunjaegg0902@naver.com>
2023-08-27 16:05:52 +0900

    Initial Commit

---
+++ MyFirstSwiftApp/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  MyFirstSwiftApp
+//
+//  Created by SUNJAE on 8/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let name = "sunjae"
+    var age = 20
+    let height: Double = 180.4
+    let status: Bool = true //상태이므로 description 사용->문자로 표현 가능
+    var body: some View {
+        VStack {
+            Text("name: \(name)")
+            Text("age: \(age)")
+            Text("height: \(height)")
+            Text("status: \(status.description)")
+          
+           
+            
+        }
+        
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFirstSwiftApp/MyFirstSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftAppApp.swift
+//  MyFirstSwiftApp
+//
+//  Created by SUNJAE on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
