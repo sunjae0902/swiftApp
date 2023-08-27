9be47a966b7db179c8e77111aa18d2e5d0c7b0cd
sunjae0902 <sunjaegg0902@naver.com>
2023-08-27 16:09:11 +0900

    first commit

--- MyFirstSwiftApp/ContentView.swift
+++ MyFirstSwiftApp/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     let name = "sunjae"
-    var age = 20
+    var age = 21
     let height: Double = 180.4
     let status: Bool = true //상태이므로 description 사용->문자로 표현 가능
     var body: some View {
